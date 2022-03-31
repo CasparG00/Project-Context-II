dad40ad785c1877601765f475d59706cbad678c7
CasparG <caspar.gelderman@gmail.com>
2022-03-31 16:13:57 +0200

    death

--- Assets/Scripts/YarnCommands/MoveNPC.cs
+++ Assets/Scripts/YarnCommands/MoveNPC.cs
@@ -4,18 +4,8 @@ using Yarn.Unity;
 public class MoveNPC
 {
     [YarnCommand("Move")]
-    public static void Move(GameObject character, GameObject destination, float speed)
+    public static void Move(GameObject character, GameObject destination)
     {
-        var tf = character.transform;
-        var target = destination.transform.position;
-
-        if (Vector2.Distance(tf.position, target) > 0.05f)
-        {
-            tf.position = Vector2.MoveTowards(tf.position, target, speed);
-        }
-        else
-        {
-            tf.position = target;
-        }
+        character.transform.position = destination.transform.position;
     }
 }
